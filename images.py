b10fd6c59fc574672d1f09765e984580c3c23205
aNOOBis <69043738+aNOOBisTheGod@users.noreply.github.com>
2021-04-29 19:44:31 +0300

    Update images.py

--- images.py
+++ images.py
@@ -42,3 +42,52 @@ def parser(x, y):
             print('Error')
 
     return parse()
+
+
+def get_html(url, params=None):
+    r = requests.get(url, params=params)
+    return r
+
+
+def chain(text):
+    def get_content(html):
+        global y
+        s = BeautifulSoup(html, 'html.parser')
+        react = s.findAll('div', class_='reac')
+        for r in react:
+            reaction = r.get_text()
+            el = str(reaction).split('→')
+            el = str(reaction).split('+')
+            el = str(reaction).split()
+            if el[0] == x or el[2] == x:
+                totr = reaction
+                break
+        try:
+            return totr
+        except:
+             return '?'
+
+
+    def parse():
+        html = get_html(url)
+        if html.status_code == 200:
+            return get_content(html.text)
+        else:
+            print('Error')
+
+
+    out = []
+    c = 0
+    a = list(text.split())
+    for i in range(len(a) - 1):
+        c += 1
+        print(out)
+        x = a[i]
+        y = a[i + 1]
+        url = 'https://tutata.ru/chemistry/search?s=%3D+' + y
+        res = parse()
+        if len(res) == 1:
+            out.append(str(c) + ':  ' + '?')
+        else:
+            out.append(str(c) + ':  ' + ''.join(res)[1:])
+    return out
